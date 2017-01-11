be5e9b82eb8a282caa9070db546f4620ca554570
steffenSuess <steffen.suess@hs-ansbach.de>
2017-01-11 11:54:08 +0100

    Initial Commit

---
+++ ToDo-List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo-List
+//
+//  Created by Steffen Süß on 11.01.17.
+//  Copyright © 2017 Steffen Süß. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
